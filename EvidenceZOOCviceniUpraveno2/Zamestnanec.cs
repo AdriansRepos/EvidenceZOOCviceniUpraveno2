6a5cc8587861bd39b1c945ff1c0ec58a6ffdd662
AdriansRepos <ferencik.adrian@outlook.cz>
2026-04-16 16:57:04 +0200

    Doplnění potřebných metod pro načítaní a zápis ze souboru

--- EvidenceZOOCviceniUpraveno2/Zamestnanec.cs
+++ EvidenceZOOCviceniUpraveno2/Zamestnanec.cs
@@ -61,6 +61,41 @@ namespace EvidenceZOOCviceniUpraveno2
             Console.WriteLine("\tMzda zaměstnance: {0}", Mzda);
 
             Console.WriteLine("\tPracovní pozice zaměstnance: {0}", PracovniPozice);
-        }                
+        }
+
+        public string ToFileString()
+        {
+            return $"{Jmeno};{Prijmeni};{Mzda}";
+        }
+
+        public static Zamestnanec Parse(string line)
+        {
+            string[] parts = line.Split(';');
+
+            if (parts.Length != 5)
+                throw new FormatException("Řádek nemá správný formát pro Zamestnance.");
+
+            if (string.IsNullOrWhiteSpace(parts[0]))
+                throw new FormatException("Jméno nesmí být prázdné.");
+            string jmeno = parts[0];
+
+            if (string.IsNullOrWhiteSpace(parts[1]))
+                throw new FormatException("Příjmení nesmí být prázdné.");
+            string prijmeni = parts[1];
+
+            if (!DateOnly.TryParse(parts[2], out DateOnly datumNarozeni))
+                throw new FormatException("Datum narození není platné.");
+
+            if (!int.TryParse(parts[3], out int mzda))
+                throw new FormatException("Mzda není platné číslo.");
+
+            if (string.IsNullOrWhiteSpace(parts[4]))
+                throw new FormatException("Pracovní pozice nesmí být prázdná.");
+            string pracovniPozice = parts[4];
+
+
+            return new Zamestnanec(jmeno, prijmeni, datumNarozeni, mzda, pracovniPozice);
+        }
+
     }
 }
