0df9e598306d25688f211a3fa228f7abba32ae06
AdriansRepos <ferencik.adrian@outlook.cz>
2026-07-10 14:49:39 +0200

    Vytvoření enum třídy se specifikací skladových pohybů položek ve skladu.

---
+++ EvidenceZOOCviceniUpraveno2/TypPohybu.cs
@@ -0,0 +1,11 @@
+﻿namespace EvidenceZOOCviceniUpraveno2
+{
+    /// <summary>
+    /// Typ skladového pohybu.
+    /// </summary>
+    enum TypPohybu
+    {
+        Naskladneni,
+        Vyskladneni
+    }
+}
