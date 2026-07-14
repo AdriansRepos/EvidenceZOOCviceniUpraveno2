754aabf6e6305c0cfe93ef736b7abe9f59251448
AdriansRepos <ferencik.adrian@outlook.cz>
2026-07-14 12:48:24 +0200

    Přidána třída s enumem pro Typ akce zaznamenané v auditním logu.

---
+++ EvidenceZOOCviceniUpraveno2/TypAkce.cs
@@ -0,0 +1,12 @@
+﻿namespace EvidenceZOOCviceniUpraveno2
+{
+    /// <summary>
+    /// Typ akce zaznamenané v auditním logu.
+    /// </summary>
+    enum TypAkce
+    {
+        Pridano,
+        Upraveno,
+        Smazano
+    }
+}
