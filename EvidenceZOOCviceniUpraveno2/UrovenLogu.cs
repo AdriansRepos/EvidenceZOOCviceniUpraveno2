cb33e434df4856cc0f5d84fa3f5ce97c8e49bcaf
AdriansRepos <ferencik.adrian@outlook.cz>
2026-07-14 12:44:05 +0200

    Přidání třídy s enumem pro určení závažnosti v technickém logu.

---
+++ EvidenceZOOCviceniUpraveno2/UrovenLogu.cs
@@ -0,0 +1,12 @@
+﻿namespace EvidenceZOOCviceniUpraveno2
+{
+    /// <summary>
+    /// Závažnost záznamu v technickém logu.
+    /// </summary>
+    enum UrovenLogu
+    {
+        Info,
+        Varovani,
+        Chyba
+    }
+}
