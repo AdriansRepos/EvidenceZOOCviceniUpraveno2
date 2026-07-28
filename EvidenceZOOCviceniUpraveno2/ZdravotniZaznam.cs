a66686585f6cbc48be92a6a19ff08ecdb6c39cfb
AdriansRepos <ferencik.adrian@outlook.cz>
2026-07-29 01:36:21 +0200

    Úprava konstruktoru.

--- EvidenceZOOCviceniUpraveno2/ZdravotniZaznam.cs
+++ EvidenceZOOCviceniUpraveno2/ZdravotniZaznam.cs
@@ -5,19 +5,15 @@ namespace EvidenceZOOCviceniUpraveno2
     /// <summary>
     /// Jeden zdravotní záznam zvířete (nemoc, léčba, podaný lék, kontrola).
     /// </summary>
-    class ZdravotniZaznam
+    [method: JsonConstructor]    /// <summary>
+                                 /// Jeden zdravotní záznam zvířete (nemoc, léčba, podaný lék, kontrola).
+                                 /// </summary>
+    class ZdravotniZaznam(DateOnly datum, string popis)
     {
         [JsonPropertyName("datum")]
-        public DateOnly Datum { get; internal set; }
+        public DateOnly Datum { get; internal set; } = datum;
 
         [JsonPropertyName("popis")]
-        public string Popis { get; internal set; } = string.Empty;
-
-        [JsonConstructor]
-        public ZdravotniZaznam(DateOnly datum, string popis)
-        {
-            Datum = datum;
-            Popis = popis;
-        }
+        public string Popis { get; internal set; } = popis;
     }
 }
