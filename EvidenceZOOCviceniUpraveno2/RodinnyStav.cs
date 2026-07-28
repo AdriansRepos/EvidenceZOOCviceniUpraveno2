22297f157a0c8725f3a78cc82f028df4491c9d4b
AdriansRepos <ferencik.adrian@outlook.cz>
2026-07-29 01:38:32 +0200

    Přidání enumu s rodinným stavem pro zápis do dat zaměstnance.

---
+++ EvidenceZOOCviceniUpraveno2/RodinnyStav.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace EvidenceZOOCviceniUpraveno2
+{
+    enum RodinnyStav
+    {
+        [Description("Svobodný/á")]
+        Svobodny,
+
+        [Description("Ženatý/vdaná")]
+        ZenatyVdana,
+
+        [Description("Rozvedený/á")]
+        Rozvedeny,
+
+        [Description("Vdovec/vdova")]
+        Vdovec
+    }
+}
