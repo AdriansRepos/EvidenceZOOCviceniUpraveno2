31dcdfdc7a3d4316fa7381beb7d5c184c34e3b1b
AdriansRepos <ferencik.adrian@outlook.cz>
2026-07-29 01:28:36 +0200

    Přidána třída pro zdravotní záznamy zvířat.

---
+++ EvidenceZOOCviceniUpraveno2/ZdravotniZaznam.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EvidenceZOOCviceniUpraveno2
+{
+    /// <summary>
+    /// Jeden zdravotní záznam zvířete (nemoc, léčba, podaný lék, kontrola).
+    /// </summary>
+    class ZdravotniZaznam
+    {
+        [JsonPropertyName("datum")]
+        public DateOnly Datum { get; internal set; }
+
+        [JsonPropertyName("popis")]
+        public string Popis { get; internal set; } = string.Empty;
+
+        [JsonConstructor]
+        public ZdravotniZaznam(DateOnly datum, string popis)
+        {
+            Datum = datum;
+            Popis = popis;
+        }
+    }
+}
