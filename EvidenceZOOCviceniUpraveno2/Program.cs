0145ecc418c10865fefb3fe21807008176dbeb91
AdriansRepos <Ferencik.Adrian@outlook.cz>
2026-08-23 18:52:54 +0200

    Vytažení menu do namespace Menu a třídy určené pro hlavní menu. Tato třída zůstane nadále pouze vstupním a inicializačním bodem aplikace.

--- EvidenceZOOCviceniUpraveno2/Program.cs
+++ EvidenceZOOCviceniUpraveno2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using EvidenceZOOCviceniUpraveno2.Logika;
-using EvidenceZOOCviceniUpraveno2.Data;
 using EvidenceZOOCviceniUpraveno2.Zaloha;
+using EvidenceZOOCviceniUpraveno2.Data.Repozitare;
+using EvidenceZOOCviceniUpraveno2.Entity;
 
 string korenovaSlozka = KonfiguraceRepository.NactiNeboSeZeptejNaCesty();
 Zoo zoo = new(korenovaSlozka);
@@ -22,68 +23,5 @@ SpravceSkladu spravceSkladu = new(zoo);
 SpravcePokladny spravcePokladny = new(zoo);
 Statistiky statistiky = new(zoo);
 
-char volbaMenu;
-do
-{
-    Console.ForegroundColor = ConsoleColor.Blue;
-    Console.WriteLine("=== HLAVNÍ MENU ===");
-    Console.WriteLine("\t1. Zvířata");
-    Console.WriteLine("\t2. Zaměstnanci");
-    Console.WriteLine("\t3. Sklad");
-    Console.WriteLine("\t4. Správce pokladny");
-    Console.WriteLine("\t5. Statistiky");
-    Console.WriteLine("\t6. Zálohovat teď");
-    Console.WriteLine("\t7. Nastavit cestu k externí záloze");
-    Console.WriteLine("\tk. Konec programu");
-    Console.ResetColor();
-    Console.Write("Vyber možnost: ");
-
-    volbaMenu = char.ToLower(Console.ReadKey().KeyChar);
-    Console.WriteLine();
-
-    switch (volbaMenu)
-    {
-        case '1':
-            spravceZvirat.Menu();
-            break;
-
-        case '2':
-            spravceZamestnancu.Menu();
-            break;
-
-        case '3':
-            spravceSkladu.Menu();
-            break;
-
-        case '4':
-            spravcePokladny.Menu();
-            break;
-
-        case '5':
-            statistiky.MenuStatistiky();
-            break;
-
-        case '6':
-            new ZalohovaciSluzba(zoo).ProvedRucniZalohu();
-            break;
-
-        case '7':
-            Console.Write("Zadej cestu k externí záloze (prázdné = vypnout): ");
-            string cesta = Console.ReadLine()!.Trim();
-            zoo.Konfigurace.NastavitExterniZalohuCestu(cesta);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Nastavení uloženo.");
-            Console.ResetColor();
-            break;
-
-        case 'k':
-            break;
-
-        default:
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Neplatná volba, opakujte zadání:");
-            Console.ResetColor();
-            break;
-    }
-}
-while (volbaMenu != 'k');
+// není ukončený a doladěný - dokončení a odladění proběhne na konci refaktoringu,
+// kdy budu kontrolovat i jednotlivá provázání namespace detailněji
