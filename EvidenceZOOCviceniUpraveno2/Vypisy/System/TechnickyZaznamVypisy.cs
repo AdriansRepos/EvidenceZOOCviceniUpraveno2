37339acc0df48e28d399bc9b0400ec3b997dce05
AdriansRepos <Ferencik.Adrian@outlook.cz>
2026-08-24 17:22:55 +0200

    Vytvoření namespace a třídy pro výpis - očištění datové třídy

---
+++ EvidenceZOOCviceniUpraveno2/Vypisy/System/TechnickyZaznamVypisy.cs
@@ -0,0 +1,21 @@
+﻿using EvidenceZOOCviceniUpraveno2.Entity;
+using EvidenceZOOCviceniUpraveno2.Enumy;
+
+namespace EvidenceZOOCviceniUpraveno2.Vypisy.System
+{
+    static class TechnickyZaznamVypisy
+    {
+        public static void VypisTechnickyZaznam(TechnickyZaznam zaznam)
+        {
+            Console.ForegroundColor = zaznam.Uroven switch
+            {
+                UrovenLogu.Chyba => ConsoleColor.Red,
+                UrovenLogu.Varovani => ConsoleColor.Yellow,
+                _ => ConsoleColor.Gray
+            };
+
+            Console.WriteLine($"{zaznam.Cas,-20:g} [{zaznam.Uroven}] {zaznam.Zprava}");
+            Console.ResetColor();
+        }
+    }
+}
