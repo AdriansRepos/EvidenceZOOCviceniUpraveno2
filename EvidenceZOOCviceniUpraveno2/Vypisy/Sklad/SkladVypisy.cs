32862a139413f13f56ca0f93744e5995620028f5
AdriansRepos <Ferencik.Adrian@outlook.cz>
2026-08-24 15:54:24 +0200

    Vytvoření namespace a třídy pro samotné výpisy

---
+++ EvidenceZOOCviceniUpraveno2/Vypisy/Sklad/SkladVypisy.cs
@@ -0,0 +1,47 @@
+﻿using BarevneVypisyHelper;
+using EvidenceZOOCviceniUpraveno2.Entity;
+using EvidenceZOOCviceniUpraveno2.Enumy;
+using PohybHelper;
+
+namespace EvidenceZOOCviceniUpraveno2.Vypisy.Sklad
+{
+    internal static class SkladVypisy
+    {
+        public static void VypisHlavickuSkladu()
+        {
+            Console.WriteLine("STAV SKLADU");
+            Console.WriteLine();
+            Console.WriteLine($"{"Název",-20} {"Kategorie",-25} {"Množství",8} {"Jednotka",-8}");
+            Console.WriteLine(new string('-', 70));
+        }
+
+        public static void VypisPolozku(SkladovaPolozka polozka)
+        {
+            if (polozka.JeDochazejici)
+                Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine(
+                $"{polozka.Nazev,-20} {polozka.Kategorie,-25} {polozka.Mnozstvi,8:0.##} {polozka.Jednotka,-8} " +
+                $"(min. {polozka.MinimalniStav:0.##})"
+            );
+
+            Console.ResetColor();
+        }
+
+        public static void VypisHlavickuPohybu()
+        {
+            Console.WriteLine($"{"Datum a čas",-20} {"Popis",-45}");
+            Console.WriteLine(new string('-', 65));
+        }
+
+        public static void VypisPohyb(SkladovyPohyb pohyb)
+        {
+            Console.ForegroundColor = pohyb.TypPohybu == SkladovyTypPohybu.Naskladneni
+                ? ConsoleColor.Green
+                : ConsoleColor.Yellow;
+
+            Console.WriteLine($"{pohyb.DatumCas,-20:g} {pohyb.Popis(),-45}");
+            Console.ResetColor();
+        }
+    }
+}
