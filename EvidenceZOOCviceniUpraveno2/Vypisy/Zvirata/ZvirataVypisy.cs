5d726d71ee33cfe3f2cfd8e77d8f6d434e25c788
AdriansRepos <Ferencik.Adrian@outlook.cz>
2026-08-24 15:16:11 +0200

    Vytvoření namespace Zvirata s třídou ZvirataVypisy pro výpisy a vložení všech výpisů do této třídy pro přehlednost

---
+++ EvidenceZOOCviceniUpraveno2/Vypisy/Zvirata/ZvirataVypisy.cs
@@ -0,0 +1,27 @@
+﻿using EvidenceZOOCviceniUpraveno2.Entity;
+
+namespace EvidenceZOOCviceniUpraveno2.Vypisy.Zvirata
+{
+    internal static class ZvirataVypisy
+    {
+        public static void VypisHlavicku()
+        {
+            Console.WriteLine("VÝPIS ZVÍŘAT");
+            Console.WriteLine();
+            Console.WriteLine($"{"Název",-20} {"Věk",-15} {"Váha",-10}");
+            Console.WriteLine(new string('-', 45));
+        }
+
+        public static void VypisZvire(Zvire zvire)
+        {
+            Console.WriteLine(
+                $"{zvire.Nazev,-20} {zvire.Vek,-15} {zvire.Vaha + " kg",-10}"
+            );
+        }
+
+        public static void VypisVyhledaneZvire(Zvire zvire)
+        {
+            Console.WriteLine($"Nalezeno:\t{zvire.Nazev}\tVěk: {zvire.Vek}\tVáha: {zvire.Vaha} kg");
+        }
+    }
+}
