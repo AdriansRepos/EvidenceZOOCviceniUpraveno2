3cd69cbad3dfe25daab5e5818674308ab20125f6
AdriansRepos <Ferencik.Adrian@outlook.cz>
2026-08-24 15:04:56 +0200

    Vytvoření namespace Statistiky s třídou StatistikyVypis pro výpisy a vložení všech výpisů do této třídy pro přehlednost

---
+++ EvidenceZOOCviceniUpraveno2/Vypisy/Statistiky/StatistikyVypisy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace EvidenceZOOCviceniUpraveno2.Vypisy.Statistiky
+{
+    internal static class StatistikyVypisy
+    {
+        public static void VypisPocetZvirat(int pocet)
+        {
+            Console.WriteLine($"Počet zvířat: {pocet}");
+        }
+
+        public static void VypisPocetZamestnancu(int pocet)
+        {
+            Console.WriteLine($"Počet zaměstnanců: {pocet}");
+        }
+
+        public static void VypisSoucetMezd(int soucet)
+        {
+            Console.WriteLine($"Součet mezd: {soucet} Kč");
+        }
+
+        public static void VypisNavstevnost(int mesic, int rok, double navstevnost)
+        {
+            Console.WriteLine($"Průměrná denní návštěvnost za {mesic}/{rok}: {navstevnost:0.##} osob/den");
+        }
+    }
+}
